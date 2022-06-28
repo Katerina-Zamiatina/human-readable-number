6e5fb268db853485d5525a7e263572e393beaacb
Katerina Zamiatina <eozamyatina@gmail.com>
2022-06-28 10:27:04 +0200

    feat: transforms numbers inder 100

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const toTwenty = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    let readable;
+    if (number < 20) {
+        readable = toTwenty[number];
+        return readable;
+    }
+    if (number > 20) {
+        let n = number.toString().split("");
+        console.log("n: ", n);
+        readable = tens[n[0]] + " " + toTwenty[n[1]];
+
+        return readable;
+    }
+    return readable;
+};
